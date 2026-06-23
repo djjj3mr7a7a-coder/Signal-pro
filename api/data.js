62e2827e8c56539707ab5c5107f4d0b9854061c7
djjj3mr7a7a-coder <djjj3mr7a7a@gmail.com>
2026-06-23 13:00:26 +0300

    Create data.js

---
+++ api/data.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const { symbol, interval } = req.query;
+  const API_KEY = "fbaf04b411c7458f923c05ef24bcc878";
+  try {
+    const url = `https://api.twelvedata.com/time_series?symbol=${symbol}&interval=${interval}&outputsize=100&apikey=${API_KEY}`;
+    const r = await fetch(url);
+    const data = await r.json();
+    res.status(200).json(data);
+  } catch(e) {
+    res.status(500).json({ error: e.message });
+  }
+}
